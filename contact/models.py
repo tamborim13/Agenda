5097d15b60bc87c9591792c3f93e8f37d4e34b76
tamborim13 <tamborimgiovanni2017@gmail.com>
2025-08-16 14:47:04 -0300

    models commit

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+# aqui é onde fica as tabelas do mysql, inves de criar la, voce usa aqui!
+
+# Todos esse campos aqui que estou criando sao obrigatorio
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=250, blank=True)
+    create_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
