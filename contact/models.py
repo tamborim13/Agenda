f2794e2cc4c59d46f2bd2fedc3b9e2f97d3a65ab
tamborim13 <tamborimgiovanni2017@gmail.com>
2025-08-17 23:34:54 -0300

    statics/media commit

--- contact/models.py
+++ contact/models.py
@@ -12,4 +12,9 @@ class Contact(models.Model):
     email = models.EmailField(max_length=250, blank=True)
     create_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default =True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
