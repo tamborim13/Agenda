008cee399bd7323a92caeea2e73387f7bcc587d0
tamborim13 <tamborimgiovanni2017@gmail.com>
2025-09-01 00:00:02 -0300

    add template

--- contact/urls.py
+++ contact/urls.py
@@ -4,6 +4,7 @@ from contact import views
 
 app_name = 'contact'
 
-urlpatterns = [
+urlpatterns = [ #Quanto mais especifica a url coloca ela pra cima.
+    path('<int:contact_id>/', views.contact, name='contact'), #Porque contact_id? porque esta lá na view!
     path('', views.index, name='index'),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+from django.http import Http404
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects\
+        .filter(show = True)\
+        .order_by('-id') [10:20]
+        
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+def contact(request, contact_id):
+    #single_contact = Contact.objects.filter(pk = contact_id).first()
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show = True
+        )
+
+    #if single_contact is None:
+       #raise Http404()
+        
+    site_title = f'{single_contact.first_name}{single_contact.last_name} - '
+
+    context = {
+        'contact': single_contact,
+        'site_title': site_title
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
