226b3dde44ca7654632ef728856e4aeffc6e8726
tamborim13 <tamborimgiovanni2017@gmail.com>
2025-09-14 23:17:03 -0300

    commit forms valid

--- contact/forms.py
+++ contact/forms.py
@@ -23,7 +23,11 @@ class ContactForm(forms.ModelForm):
 
     class Meta:
         model = models.Contact
-        fields = 'first_name', 'last_name', 'phone'
+        fields = (
+            'first_name', 'last_name', 'phone',
+            'email', 'description', 'category',
+        )
+        
 
         # widgets = {
         #     'first_name': forms.TextInput(
@@ -35,20 +39,31 @@ class ContactForm(forms.ModelForm):
         # }
 
     def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-        self.add_error(
-            'last_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+        if first_name == last_name:
+            msg = ValidationError(
+                    'Não pode ser iguais os nomes',
+                    code= 'invalid'
+                    )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
 
         return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Veio do add_error',
+                    code='invalid'
+                )
+            )
+
+        return first_name
