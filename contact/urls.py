00d1d9c6c319ffe50d70fa696f4078b7f12f9529
tamborim13 <tamborimgiovanni2017@gmail.com>
2025-09-15 13:42:26 -0300

    update form

--- contact/urls.py
+++ contact/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [ #Quanto mais especifica a url coloca ela pra cima.
     #Contact(CRUD)
     path('contact/<int:contact_id>/', views.contact, name='contact'), #Porque contact_id? porque esta lá na view!
     path('contact/create/', views.create, name='create'), #Porque contact_id? porque esta lá na view!
+    path('contact/<int:contact_id>/update/', views.update, name='update'), #Porque contact_id? porque esta lá na view!
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'), #Porque contact_id? porque esta lá na view!
 ]
